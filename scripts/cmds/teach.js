5b22201150c36468dd79b2f2ff86a694227fb60f
khlea-ishere01 <khleajazz@gmail.com>
2024-07-18 11:14:04 +0800

    Update teach.js

--- scripts/cmds/teach.js
+++ scripts/cmds/teach.js
@@ -1,24 +1,24 @@
 const axios = require("axios");
 
-module.exports = {
-    config: {
-        name: "teach",
-        version: "1.0.0",
-        hasPermission: 0,
-        credits: "jerome",
-        description: "Teach Simsimi",
-        usages: "Teach",
-        commandCategory: "...",
-        cooldowns: 0
-    },
-    onStart: async ({ api, event, args }) => {
+module.exports.config = {
+    name: "teach",
+    version: "1.0.0",
+    role: 0,
+    author: "jerome",
+    description: "Teach Simsimi",
+    usages: "Teach",
+    category: "...",
+    cooldowns: 0
+};
+
+module.exports.onStart = async ({ api, event, args }) => {
     try {
         const text = args.join(" ");
         const text1 = text.substr(0, text.indexOf(' - '));
         const text2 = text.split(" - ").pop();
 
         if (!text1 || !text2) {
-            return api.sendMessage(`Usage: _teach hi - hello`, event.threadID, event.messageID);
+            return api.sendMessage(`Usage: ${global.config.PREFIX}teach hi - hello`, event.threadID, event.messageID);
         }
 
         const response = await axios.get(`https://simsimi-api-pro.onrender.com/teach?ask=${encodeURIComponent(text1)}&ans=${encodeURIComponent(text2)}`);
@@ -32,5 +32,4 @@ module.exports = {
         console.error("An error occurred:", error);
         api.sendMessage("Oops! Something went wrong.", event.threadID, event.messageID);
     }
-}
 };
