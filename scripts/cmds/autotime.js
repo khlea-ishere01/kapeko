4c7e21fd5b36d22090f21d54161fdfafcd492655
khlea-ishere01 <khleajazz@gmail.com>
2024-07-26 14:53:16 +0800

    Update autotime.js

--- scripts/cmds/autotime.js
+++ scripts/cmds/autotime.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "autotime",
   version: "2.0.0",
   role: 0,
-  author: "pogi", //don't change the author kung ayaw mong ma pwetan!!
+  author: "kylepogi", // ninakaw ni jay heheheh laplapin kita!! 
   description: "Automatically sends messages based on set times.",
   category: "AutoTime",
   countDown: 3
@@ -13,28 +13,26 @@ module.exports.config = {
 module.exports.onLoad = async ({ api }) => {
   const arrayData = {
     "06:00:00 AM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 06:00 AM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "09:00:00 AM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 09:00 AM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "12:00:00 PM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 12:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "03:00:00 PM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 03:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "06:00:00 PM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 06:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "09:00:00 PM": {
-      message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 09:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
-    },
-    "10:00:00 PM": {
-      message: "TIMECHECK: 10:00 PM\nGood night, everyone!"
-    }
-
-    // Add more messages for other times as needed
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 06:00 AM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "09:00:00 AM": {
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 09:00 AM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "12:00:00 PM": {
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 12:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "03:00:00 PM": {
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 03:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "06:00:00 PM": {
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 06:00 PM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "09:00:00 PM": {
+        message: "THIS IS AN AUTOMATED MESSAGE \n▬▬▬▬▬▬▬▬▬▬▬▬\nTIMECHECK: 09:00 AM\n\n Use the command #educgc to be added to Azry's educ/music/fun room! Please add friend bot first before using command."
+      },
+      "10:00:00 PM": {
+        message: "TIMECHECK: 10:00 PM\nGood night, everyone!"
+      }
   };
 
   const checkTimeAndSendMessage = () => {
